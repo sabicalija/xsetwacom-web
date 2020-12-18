7bcc26aac2dad9f5071aaf8232f40ebf81866c13
Alija Sabic <sabic.alija@gmail.com>
2020-12-18 01:44:40 +0100

    server: add device overview endpoint

--- packages/xsetwacom-server/controllers/device.js
+++ packages/xsetwacom-server/controllers/device.js
@@ -5,6 +5,10 @@ const getAll = async (req, res, next) => {
   res.status(200).json(await getAllDevices());
 };
 
+const getDeviceOverview = async (req, res, next) => {
+  res.status(200).json(await getDeviceData(req.params));
+};
+
 async function getAllDevices() {
   let result = [];
   try {
@@ -25,4 +29,19 @@ async function getAllDevices() {
   return result;
 }
 
-module.exports = { getAll };
+async function getDeviceData({ id }) {
+  let result = {};
+  try {
+    const { stdout } = await exec(`xsetwacom --get ${id} all`);
+    const options = stdout.split("\n");
+    for (option of options) {
+      try {
+        const [m, name, value] = /^Option\s+\"(.*)\"\s\"(.*)\"$/.exec(option);
+        result[name] = value;
+      } catch {}
+    }
+  } catch {}
+  return result;
+}
+
+module.exports = { getAll, getDeviceOverview };
--- packages/xsetwacom-server/routes/device.js
+++ packages/xsetwacom-server/routes/device.js
@@ -4,5 +4,6 @@ const deviceController = require("../controllers/device.js");
 const router = Router();
 
 router.get("/all", deviceController.getAll);
+router.get("/:id/overview", deviceController.getDeviceOverview);
 
 module.exports = router;
